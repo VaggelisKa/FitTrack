084daab73339729fa976169e5fe39897ce74ce34
LoadedGun <vaggos98@gmail.com>
2020-04-09 15:00:45 +0300

    Adding first form sections

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
+import {
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    } from '@angular/material';
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule
+    ]
 })
 export class MaterialModule {}

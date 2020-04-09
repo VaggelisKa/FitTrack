6aa335b81fb339a82fd609c451beb24ae500ce14
LoadedGun <vaggos98@gmail.com>
2020-04-09 18:56:52 +0300

    Adding Custom Datepicker

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormControl } from '@angular/forms';
+import moment from 'moment';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+   date = new FormControl(moment());
 
   ngOnInit() {
   }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,20 +4,57 @@ import {
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
+    MatDatepickerModule,
     } from '@angular/material';
+
+
+// IMPORTS FOR CUSTOM DATEPICKER
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import * as _moment from 'moment';
+import {default as _rollupMoment} from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 
 @NgModule({
     imports: [
         MatButtonModule,
         MatFormFieldModule,
         MatInputModule,
-        MatIconModule
+        MatIconModule,
+        MatDatepickerModule
     ],
     exports: [
         MatButtonModule,
         MatFormFieldModule,
         MatInputModule,
-        MatIconModule
-    ]
+        MatIconModule,
+        MatDatepickerModule,
+        MatMomentDateModule
+    ],
+
+    providers: [
+        {
+          provide: DateAdapter,
+          useClass: MomentDateAdapter,
+          deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+        },
+        {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+      ],
 })
 export class MaterialModule {}

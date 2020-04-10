98fa69528c8dfdc45ca89d4d5411b8747432313c
VaggelisKa <e.karavasileiadis@gmail.com>
2020-04-10 18:22:35 +0300

    Changing the sidenav open toggle logic using local reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isSidenavOpen = false;
-
-  controlSidenav() {
-    this.isSidenavOpen = !this.isSidenavOpen;
-  }
+  isSidenavOpen =  false;
 }

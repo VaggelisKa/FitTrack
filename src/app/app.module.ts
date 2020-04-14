0ea79d170575296e4fd17962e33c25a903eb9207
VaggelisKa <e.karavasileiadis@gmail.com>
2020-04-14 13:33:01 +0300

    Setting up angular fire and getting exercises from firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +21,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
 import { StopTrainingComponent } from './training/current-training/stop.training.component';
 import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -32,7 +35,7 @@ import { TrainingService } from './training/training.service';
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
-    StopTrainingComponent
+    StopTrainingComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,7 +43,9 @@ import { TrainingService } from './training/training.service';
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [AuthService, TrainingService],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBHqYB96VxJ1ujBKNmhI8fw4LJtWzP01Zk',
+    authDomain: 'fittrack-1bb19.firebaseapp.com',
+    databaseURL: 'https://fittrack-1bb19.firebaseio.com',
+    projectId: 'fittrack-1bb19',
+    storageBucket: 'fittrack-1bb19.appspot.com',
+    messagingSenderId: '986089102477',
+    appId: '1:986089102477:web:81fda674a93466b4aed6c4',
+    measurementId: 'G-4ZHP2GXZKD'
+  }
 };
 
 /*

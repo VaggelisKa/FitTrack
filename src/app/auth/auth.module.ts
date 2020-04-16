07ea7d4b2db0451d1348cc06cf573634314c098e
VaggelisKa <e.karavasileiadis@gmail.com>
2020-04-16 14:00:35 +0300

    Spliting the app into more modules (Auth / Training Modules)

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+
+
+@NgModule({
+    declarations: [
+        SignupComponent,
+        LoginComponent
+    ],
+
+    imports: [
+        CommonModule,
+        FormsModule,
+        MaterialModule,
+        FlexLayoutModule
+    ],
+
+    exports: []
+})
+export class AuthModule {}

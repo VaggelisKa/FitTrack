5cf8345124c84fa2295b657a9ac8217e26c91524
VaggelisKa <e.karavasileiadis@gmail.com>
2020-04-16 21:52:46 +0300

    Copying the firebase access keys over to production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBHqYB96VxJ1ujBKNmhI8fw4LJtWzP01Zk',
+    authDomain: 'fittrack-1bb19.firebaseapp.com',
+    databaseURL: 'https://fittrack-1bb19.firebaseio.com',
+    projectId: 'fittrack-1bb19',
+    storageBucket: 'fittrack-1bb19.appspot.com',
+    messagingSenderId: '986089102477',
+    appId: '1:986089102477:web:81fda674a93466b4aed6c4',
+    measurementId: 'G-4ZHP2GXZKD'
+  }
 };
